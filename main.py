236c3e92392c3dacfa0e38749322ba9dd0f33197
Jurica Slovinac <jurs.slovinac2@gmail.com>
2023-06-25 16:53:21 +0200

    Added function for binding pod to a node

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from kubernetes import client
+from kubernetes import client, watch
 from modules.predictions import make_prediction
 from modules.kubernetes_telemetry import get_nodes
 
@@ -11,7 +11,6 @@ configuration.verify_ssl = False
 
 ApiClient = client.ApiClient(configuration)
 
-v1apps = client.AppsV1Api(ApiClient)
 v1core = client.CoreV1Api(ApiClient)
 api_custom = client.CustomObjectsApi(ApiClient)
 
@@ -19,5 +18,32 @@ api_custom = client.CustomObjectsApi(ApiClient)
 # Get list of all worker nodes
 nodes = get_nodes(v1core.list_node(label_selector="node=worker"))
 
-nodes_set = make_prediction(nodes, v1core.list_node(label_selector="node=worker"), api_custom.list_cluster_custom_object(group="metrics.k8s.io",version="v1beta1", plural="nodes"))
-print(nodes_set)
+#nodes_set = make_prediction(nodes, v1core.list_node(label_selector="node=worker"), api_custom.list_cluster_custom_object(group="metrics.k8s.io",version="v1beta1", plural="nodes"))
+#print(nodes_set)
+
+def schedule(name, node, namespace="default"):    
+    target=client.V1ObjectReference()
+    target.kind="Node"
+    target.apiVersion="v1"
+    target.name=node
+
+    meta=client.V1ObjectMeta()
+    meta.name=name
+
+    body=client.V1Binding(target=target, metadata=meta)
+
+    return v1core.create_namespaced_binding(namespace, body, _preload_content=False)
+
+def main():
+    w = watch.Watch()
+    print("Waiting for pods...")
+    for event in w.stream(v1core.list_namespaced_pod, 'default'):
+        if event['type'] == "ADDED" and event['object'].status.phase == "Pending" and event['object'].spec.scheduler_name == 'ml-scheduler':
+            try:
+                schedule(event['object'].metadata.name, 'minikube-m02')
+            except client.rest.ApiException as e:
+                print (e)
+    return
+
+if __name__ == '__main__':
+    main()
